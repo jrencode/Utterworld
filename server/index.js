3a9d7af3a6553f3a901ee3ee1508202097c6c4f1
jrencode <jrendelatorrejr@gmail.com>
2023-01-27 03:47:58 +0800

    some changes deplyoment

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
 const CONNECTION_URL = process.env.MONGODB_URI || MONGODB_URI
-const PORT = process.env.PORT|| 10000;
+const PORT = process.env.PORT|| 5000;
 
 mongoose.set('strictQuery', true);
 mongoose.connect(CONNECTION_URL, { 
@@ -34,7 +34,9 @@ mongoose.connect(CONNECTION_URL, {
   useUnifiedTopology: true 
 })
   .then(() => 
-  app.listen(PORT, () => 
-  console.log(`Server Running on Port: http://localhost:${PORT}`))
+  app.listen(PORT, () => (
+    console.log(`Server Running on Port: http://localhost:${PORT}`))
+  )
+  
   )
   .catch((error) => console.log(`${error} did not connect`));
