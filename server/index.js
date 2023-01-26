c371a979e77b3f7af3388ad6a251626426f978f1
jrencode <jrendelatorrejr@gmail.com>
2023-01-27 02:03:31 +0800

    some changes

--- server/index.js
+++ server/index.js
@@ -16,11 +16,17 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
+if (process.env.NODE_ENV === 'production') {
+  //*Set static folder up in production
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+}
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
 const CONNECTION_URL = process.env.MONGODB_URI || MONGODB_URI
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT|| 10000;
 
 mongoose.set('strictQuery', true);
 mongoose.connect(CONNECTION_URL, { 
