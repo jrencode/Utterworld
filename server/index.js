83b3229a73d9c56e426a515d270a972b3178ea48
jrencode <jrendelatorrejr@gmail.com>
2023-01-27 00:46:59 +0800

    simple build changes

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
-const CONNECTION_URL = process.env.MONGO_URL
+const CONNECTION_URL = process.env.MONGODB_UR
 const PORT = process.env.PORT|| 5000;
 
 mongoose.set('strictQuery', true);
