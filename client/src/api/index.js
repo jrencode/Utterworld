1f1a32ce24a6053f9f69aea98f4feaa7b5028a21
jrencode <jrendelatorrejr@gmail.com>
2023-02-07 10:04:31 +0800

    fixing crud

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 
-//const url = 'https://utterworld.onrender.com/posts'
-const url = 'http://localhost:5000/posts'
+const url = 'https://utterworld.onrender.com/posts'
+//const url = 'http://localhost:5000/posts'
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
